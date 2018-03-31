6842e71d3258a9217541fee9deb9f75764dfbf5e
zzy199477 <35500798+zzy199477@users.noreply.github.com>
2018-03-30 23:56:24 -0400

    HPM573S18_ZHANG_HW7
    HPM573S18_ZHANG_HW7

---
+++ HW7/Q2.py
@@ -0,0 +1,5 @@
+#The probability distribution “the number of participants that survived beyond 5 years in a cohort of 𝑁 participants”
+#would follow a Binomial distribution.
+
+# parameters:
+#N participants, expected number of survivals Nq, variation Nq(1-q)
---
+++ HW7/Q3.py
@@ -0,0 +1,3 @@
+from scipy.stats import binom
+
+print(binom.pmf(400,573,0.5))
